c767bee5e400ba3e778aca1b9149d5360e4a7431
“Karan <karanseth200066@gmail.com>
2022-02-07 04:24:57 +0530

    Added comments and fixed bugs

--- game.java
+++ game.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+//Made pair class to store whether the user won the game or lost the game 
 class pair {
     boolean won = false;
     boolean lost = false;
@@ -16,26 +16,26 @@ class pair {
 
 public class game {
 
-    public static boolean up(int[][] a) {
+    public static boolean up(int[][] board) {
         boolean moved = false;
         int si, ei;
         for (int j = 0; j < 4; j++) {
             si = 0;
             ei = j;
             for (int i = 1; i < 4; i++) {
-                if (a[i][j] != 0) {
-                    if (a[i - 1][j] == 0 || a[i - 1][j] == a[i][j]) {
-                        if (a[si][ei] == a[i][j]) {
-                            a[si][ei] *= 2;
-                            a[i][j] = 0;
+                if (board[i][j] != 0) {
+                    if (board[i - 1][j] == 0 || board[i - 1][j] == board[i][j]) {
+                        if (board[si][ei] == board[i][j]) {
+                            board[si][ei] *= 2;
+                            board[i][j] = 0;
                             si++;
                         } else {
-                            if (a[si][ei] == 0) {
-                                a[si][ei] = a[i][j];
-                                a[i][j] = 0;
+                            if (board[si][ei] == 0) {
+                                board[si][ei] = board[i][j];
+                                board[i][j] = 0;
                             } else {
-                                a[++si][ei] = a[i][j];
-                                a[i][j] = 0;
+                                board[++si][ei] = board[i][j];
+                                board[i][j] = 0;
                             }
                         }
                         moved = true;
@@ -47,26 +47,26 @@ public class game {
         return moved;
     }
 
-    public static boolean down(int[][] a) {
+    public static boolean down(int[][] board) {
         boolean moved = false;
         int si, ei;
         for (int j = 0; j < 4; j++) {
             si = 3;
             ei = j;
             for (int i = 2; i >= 0; i--) {
-                if (a[i][j] != 0) {
-                    if (a[i + 1][j] == 0 || a[i + 1][j] == a[i][j]) {
-                        if (a[si][ei] == a[i][j]) {
-                            a[si][ei] *= 2;
-                            a[i][j] = 0;
+                if (board[i][j] != 0) {
+                    if (board[i + 1][j] == 0 || board[i + 1][j] == board[i][j]) {
+                        if (board[si][ei] == board[i][j]) {
+                            board[si][ei] *= 2;
+                            board[i][j] = 0;
                             si--;
                         } else {
-                            if (a[si][ei] == 0) {
-                                a[si][ei] = a[i][j];
-                                a[i][j] = 0;
+                            if (board[si][ei] == 0) {
+                                board[si][ei] = board[i][j];
+                                board[i][j] = 0;
                             } else {
-                                a[--si][ei] = a[i][j];
-                                a[i][j] = 0;
+                                board[--si][ei] = board[i][j];
+                                board[i][j] = 0;
                             }
                         }
                         moved = true;
@@ -78,27 +78,26 @@ public class game {
         return moved;
     }
 
-    // 4 2 0
-    public static boolean left(int[][] a) {
+    public static boolean left(int[][] board) {
         boolean moved = false;
         int si, ei;
         for (int i = 0; i < 4; i++) {
             si = i;
             ei = 0;
             for (int j = 1; j < 4; j++) {
-                if (a[i][j] != 0) {
-                    if (a[i][j - 1] == 0 || a[i][j - 1] == a[i][j]) {
-                        if (a[si][ei] == a[i][j]) {
-                            a[si][ei] *= 2;
-                            a[i][j] = 0;
+                if (board[i][j] != 0) {
+                    if (board[i][j - 1] == 0 || board[i][j - 1] == board[i][j]) {
+                        if (board[si][ei] == board[i][j]) {
+                            board[si][ei] *= 2;
+                            board[i][j] = 0;
                             ei++;
                         } else {
-                            if (a[si][ei] == 0) {
-                                a[si][ei] = a[i][j];
-                                a[i][j] = 0;
+                            if (board[si][ei] == 0) {
+                                board[si][ei] = board[i][j];
+                                board[i][j] = 0;
                             } else {
-                                a[si][++ei] = a[i][j];
-                                a[i][j] = 0;
+                                board[si][++ei] = board[i][j];
+                                board[i][j] = 0;
                             }
                         }
                         moved = true;
@@ -110,26 +109,26 @@ public class game {
         return moved;
     }
 
-    public static boolean right(int[][] a) {
+    public static boolean right(int[][] board) {
         boolean moved = false;
         int si, ei;
         for (int i = 0; i < 4; i++) {
             si = i;
             ei = 3;
             for (int j = 2; j >= 0; j--) {
-                if (a[i][j] != 0) {
-                    if (a[i][j + 1] == 0 || a[i][j + 1] == a[i][j]) {
-                        if (a[si][ei] == a[i][j]) {
-                            a[si][ei] *= 2;
-                            a[i][j] = 0;
+                if (board[i][j] != 0) {
+                    if (board[i][j + 1] == 0 || board[i][j + 1] == board[i][j]) {
+                        if (board[si][ei] == board[i][j]) {
+                            board[si][ei] *= 2;
+                            board[i][j] = 0;
                             ei--;
                         } else {
-                            if (a[si][ei] == 0) {
-                                a[si][ei] = a[i][j];
-                                a[i][j] = 0;
+                            if (board[si][ei] == 0) {
+                                board[si][ei] = board[i][j];
+                                board[i][j] = 0;
                             } else {
-                                a[si][--ei] = a[i][j];
-                                a[i][j] = 0;
+                                board[si][--ei] = board[i][j];
+                                board[i][j] = 0;
                             }
                         }
                         moved = true;
@@ -182,6 +181,7 @@ public class game {
     }
 
     public static void display(int[][] board) {
+
         System.out.println("   Press: 1->left,2->right,3->up,4->down,n->new game,q->quit");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -196,30 +196,40 @@ public class game {
     }
 
     public static void newGame() {
+        //Initalizing board
         board = new int[4][4];
+        // As we know at the start we have to fill 2 cells with either 2 or 4 
         int r1, c1, r2, c2;
+        //Getting random values for first cell
         r1 = (int) (Math.random() * 4);
         c1 = (int) (Math.random() * 4);
         while (true) {
+        //Getting random values for second cell
             r2 = (int) (Math.random() * 4);
             c2 = (int) (Math.random() * 4);
+            //If both the cells are different then we break out of the loop else we find different cell
             if (r2 != r1 && c2 != c1)
                 break;
         }
+        //placing 2 or 4 on the 2 cells that we get from above
+        //(int) Math.pow(2, (int) (Math.random() * 2) + 1) -> it basically generating 2 or 4 randomly
         board[r1][c1] = (int) Math.pow(2, (int) (Math.random() * 2) + 1);
         board[r2][c2] = (int) Math.pow(2, (int) (Math.random() * 2) + 1);
 
     }
-
+    // Declared board
     public static int[][] board;
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("                            2048 game");
+        //Creating game 
         newGame();
         display(board);
         char ch;
         while (true) {
-            ch = s.next().charAt(0);
+            String str=s.next();
+            //if input length is greater than 1 then it's a invalid argument 
+            ch=str.length()==1?str.charAt(0):'#';
             boolean moved = false;
             if (ch - '0' == 1)
                 moved = left(board);
@@ -241,15 +251,15 @@ public class game {
                 
             else
                 System.out.println("        Enter Valid Arguments as mentioned below!!!");
-
+       // If any of the above operation is performed then we add new cell to the board
             if (moved)
                 addNewCell(board);
-            display(board);
+            //finding whether game is complete or not 
             pair p = gameCompleteOrNot(board);
             if (p.won) {
                 System.out.println("              Congratulations you won the game!!!!");
-                System.out.println("Wanna Play Again(Y/N)???");
-                if (s.next().charAt(0) == 'Y') {
+                System.out.println("Wanna Play Again(y/n)???");
+                if (s.next().charAt(0) == 'y') {
                     System.out.println("                   New Game Started");
                     newGame();
                 }
@@ -259,13 +269,14 @@ public class game {
             }
             if (!p.lost) {
                 System.out.println("                    GAME OVER");
-                System.out.println("Wanna Play Again(Y/N)???");
-                if (s.next().charAt(0) == 'Y') {
+                System.out.println("  Wanna Play Again(y/n)???");
+                if (s.next().charAt(0) == 'y') {
                     System.out.println("                   New Game Started");
                     newGame();
                 } else
                     break;
             }
+            display(board);
 
         }
         s.close();
